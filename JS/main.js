a56adafc79859f9d6e215ce33f95739e7162ff8c
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-17 13:07:25 +0100

    bomba clicked

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,46 @@
-
-// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-// Bonus
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-//  - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
+// Il computer deve generare 16 numeri casuali nello stesso range della difficolta prescelta 
+// (se avete fatto bonus di ieri e partite da li, sennò range rimane di base 1-100): le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, 
+// perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - 
+// la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e 
+// l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba 
+// o quando raggiunge il numero massimo possibile di numeri consentiti 
+// (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, 
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
 const campo = document.querySelector(".campo");
 
 const gioca = document.querySelector("#gioca");
-const difficoltà = document.getElementById("difficoltà");
+const difficolta = parseInt((document.getElementById("difficolta")).value);
+
+
+const numBombe = 16;
+// genero array con i numeri delle bombe
+const arrBombe = genArrNumUniciRandomMinMax (numBombe, 1, difficolta)
+// console.log( difficolta);
+console.log(arrBombe);
 
 gioca.addEventListener("click",
     function(){
         // svuoto il campo
         campo.innerHTML = "";
         // riempo il campo
-        generaCampo(parseInt(difficoltà.value));          
+        generaCampo(difficolta);          
+        // mostro il campo
+        campo.classList.add("visibile");
             
     }
 );
 
 
 
-
+// numTess sarebbe la difficolta
 function generaCampo(numTess){
     
     for(let i = 1; i <= numTess; i++){
@@ -44,6 +56,9 @@ function generaCampo(numTess){
             nuovaCella = newCella("difficult");
         };
 
+        if(arrBombe.includes(i)){
+            nuovaCella.classList.add("bomba");
+        };
 
         // le inserisco il suo numero
         nuovaCella.innerHTML = i;
