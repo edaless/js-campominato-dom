71233aafd6de0a3c7c7f2c625f801204879bba3e
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-26 13:10:56 +0100

    commenti

--- JS/main.js
+++ JS/main.js
@@ -2,14 +2,18 @@
 // (se avete fatto bonus di ieri e partite da li, sennò range rimane di base 1-100): le bombe.
 // Attenzione: nella stessa cella può essere posizionata al massimo una bomba, 
 // perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
 // In seguito l’utente clicca su una cella:
 // se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - 
 // la cella si colora di rosso e la partita termina.
+
 // Altrimenti la cella cliccata si colora di azzurro e 
 // l’utente può continuare a cliccare sulle altre celle.
+
 // La partita termina quando il giocatore clicca su una bomba 
 // o quando raggiunge il numero massimo possibile di numeri consentiti 
 // (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
 // Al termine della partita il software deve comunicare il punteggio, 
 // cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
@@ -17,21 +21,35 @@
 const campo = document.querySelector(".campo");
 
 const gioca = document.querySelector("#gioca");
-const difficolta = parseInt((document.getElementById("difficolta")).value);
 
+let difficolta;
 
 const numBombe = 16;
-// genero array con i numeri delle bombe
-const arrBombe = genArrNumUniciRandomMinMax (numBombe, 1, difficolta)
-// console.log( difficolta);
-console.log(arrBombe);
+// genero array in cui metterò i numeri delle bombe
+let arrBombe;
+
+let punteggio = 0;
 
 gioca.addEventListener("click",
     function(){
+
+        // prendo dal select il valore di difficolta
+        difficolta = parseInt((document.getElementById("difficolta")).value);
+        
+        // svuoto l'array
+        arrBombe = [];          
+        // riempo l'array
+        arrBombe = genArrNumUniciRandomMinMax (numBombe, 1, difficolta);  
+        console.log(arrBombe);  
+        let arrNorm = [];  
+        arrNorm = arrSano(arrBombe);  
+        
+        
         // svuoto il campo
         campo.innerHTML = "";
         // riempo il campo
-        generaCampo(difficolta);          
+        generaCampo(difficolta); 
+
         // mostro il campo
         campo.classList.add("visibile");
             
@@ -56,9 +74,7 @@ function generaCampo(numTess){
             nuovaCella = newCella("difficult");
         };
 
-        if(arrBombe.includes(i)){
-            nuovaCella.classList.add("bomba");
-        };
+        
 
         // le inserisco il suo numero
         nuovaCella.innerHTML = i;
@@ -66,10 +82,25 @@ function generaCampo(numTess){
         // le aggiungo l'event listener
         nuovaCella.addEventListener("click",
             function(){
-                // metto/tolgo la classe clicked (la rendo azzurra)
-                nuovaCella.classList.toggle("clicked");
-                // stampo il numero in console
-                console.log("cella #" + i);
+                // se il numero in questione è una bomba ne aggiungo la classe
+                if(arrBombe.includes(i)){
+                    nuovaCella.classList.add("bomba");
+                    console.log("hai perso, hai fatto " + punteggio + " punti")
+                }else{
+                    // metto/tolgo la classe clicked (la rendo azzurra)
+                    nuovaCella.classList.add("clicked");
+                    // stampo il numero in console
+                    console.log("cella #" + i);
+                    // incremento il punteggio
+                    punteggio++;
+                };
+                console.log(punteggio);
+
+                if(punteggio === difficolta - numBombe){
+                    console.log("impressionante, hai vinto")
+                    console.log("hai fatto " + punteggio + " punti")
+                };
+
             }
         );
 
@@ -94,7 +125,20 @@ function newCella(tipo){
 
 
 
+function arrSano(arrBombe){
+    let arr = [];
+    for(let i = 1; i <= difficolta; i++){
+        if(!arrBombe.includes(i)){
+            arr[i] = (i);
+        }else{
+            arr[i] = "n";
+        };
+       
+    };
+    console.log(arr);
+    return arr;
 
+}
 
 
 
